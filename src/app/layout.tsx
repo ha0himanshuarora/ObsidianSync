d1a709d909b50567279245a0e904a4d20338a528
ha0himanshuarora <ha0himanshuarora@gmail.com>
2025-02-25 21:15:11 +0530

    '

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body>
+            <ThemeProvider>
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                {children}
+            </ThemeProvider>
+        </body>
     </html>
   );
 }
