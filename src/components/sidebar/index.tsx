78e5b73b40d7c352e40b34e6cc17b762b2a4ce46
ha0himanshuarora <ha0himanshuarora@gmail.com>
2025-03-01 00:12:32 +0530

    Updated Workflow-Form and Sidebar Component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -40,7 +40,7 @@ const MenuOptions = (props: Props) => {
         </TooltipProvider>
         
         <Separator />
-        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+        {/* <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
           <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
             <LucideMousePointerClick
               className="dark:text-white"
@@ -68,7 +68,7 @@ const MenuOptions = (props: Props) => {
               size={18}
             />
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="flex items-center justify-center flex-col gap-8">
         <ModeToggle />
